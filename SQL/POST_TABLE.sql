e38ba4b5e01e1efc5cdda92daf1c2fe3ab1c6a4f
yzhng249 <yzhng249@illinois.edu>
2018-11-30 04:10:18 -0600

    post table

--- SQL/POST_TABLE.sql
+++ SQL/POST_TABLE.sql
@@ -1,10 +1,11 @@
 CREATE TABLE Post(
-	postId VARCHAR(10) PRIMARY KEY,
+	postId INT NOT NULL AUTO_INCREMENT,
 	review VARCHAR(2000),
 	rating float(2,1),
 	ImdbId VARCHAR(20),
 	movieTitle VARCHAR(255),
 	Username VARCHAR(10),
+	PRIMARY KEY (postId),
 	FOREIGN KEY (ImdbId) REFERENCES Movie(ImdbId),
 	FOREIGN KEY (Username) REFERENCES Users(Username)
 )ENGINE=MyISAM;
